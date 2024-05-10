47bc49a244e9fc08a6f6ed1837eb7f5966b2bbf2
aynakeya <aynakeya.official@gmail.com>
2024-05-10 10:39:07 +0800

    update verifier

--- lens/authentication/service/loginstatus/jwt.go
+++ lens/authentication/service/loginstatus/jwt.go
@@ -41,7 +41,7 @@ func (j *JWT) Verify(request *http.Request) (status authentication.LoginStatus, 
 		return authentication.LoginStatus{
 			UserID:   claims.UserID,
 			Token:    token.Raw,
-			Verifier: j.SrvImplName().Implementation,
+			Verifier: j.SrvImplName().Version,
 		}, nil
 	} else {
 		return status, err
@@ -64,7 +64,7 @@ func (j *JWT) Login(userId string, resp http.ResponseWriter) (status authenticat
 	return authentication.LoginStatus{
 		UserID:   userId,
 		Token:    tokenString,
-		Verifier: j.SrvImplName().Implementation,
+		Verifier: j.SrvImplName().Version,
 		ExpireAt: -1,
 	}, nil
 }
