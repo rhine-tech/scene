5d626740a2f64a40131a04cfa7722a9c62385c6c
aynakeya <aynakeya.official@gmail.com>
2024-08-03 00:37:35 +0800

    update must utils

--- utils/must/must.go
+++ utils/must/must.go
@@ -8,9 +8,18 @@ func Must3[T1 any, T2 any](val1 T1, val2 T2, err error) (T1, T2) {
 	return val1, val2
 }
 
-func MPanic[T any](val T, err error) T {
+// PMust panic if err is not nil
+func PMust[T any](val T, err error) T {
 	if err != nil {
 		panic(err)
 	}
 	return val
+}
+
+// PMust3 panic if err is not nil
+func PMust3[T1 any, T2 any](val1 T1, val2 T2, err error) (T1, T2) {
+	if err != nil {
+		panic(err)
+	}
+	return val1, val2
 }
