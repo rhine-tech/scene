67093e4c7647f61df8be6518f9606564b9440841
aynakeya <aynakeya.official@gmail.com>
2024-08-03 00:37:56 +0800

    prototype of GenericRepository

--- composition/orm/gorm.go
+++ composition/orm/gorm.go
@@ -1,6 +1,7 @@
 package orm
 
 import (
+	"github.com/rhine-tech/scene/model"
 	"github.com/rhine-tech/scene/model/query"
 	"gorm.io/gorm"
 )
@@ -10,4 +11,33 @@ type Gorm interface {
 	query.QueryBuilder[*gorm.DB]
 	DB() *gorm.DB
 	RegisterModel(model ...any) error
+}
+
+type GormRepository[Model any] struct {
+	db          Gorm
+	fieldMapper query.FieldMapper
+}
+
+func (g *GormRepository[Model]) Setup() error {
+	return g.db.RegisterModel(new(Model))
+}
+
+func (g *GormRepository[Model]) Count(options ...query.Option) (count int64, err error) {
+	err = g.db.WithFieldMapper(g.fieldMapper).Build(options...).Count(&count).Error
+	return count, err
+}
+
+func (g *GormRepository[Model]) List(offset, limit int64, options ...query.Option) (model.PaginationResult[Model], error) {
+	var result = model.PaginationResult[Model]{
+		Results: make([]Model, 0),
+	}
+	qry := g.db.WithFieldMapper(g.fieldMapper).Build(options...).Session(&gorm.Session{})
+	err := qry.Offset(int(offset)).Limit(int(limit)).Find(&result.Results).Error
+	if err != nil {
+		return model.PaginationResult[Model]{}, err
+	}
+	qry.Count(&result.Total)
+	result.Offset = offset
+	result.Count = int64(len(result.Results))
+	return result, nil
 }
--- composition/orm/orm.go
+++ composition/orm/orm.go
@@ -1,9 +1,22 @@
 package orm
 
-import "github.com/rhine-tech/scene"
+import (
+	"github.com/rhine-tech/scene"
+	"github.com/rhine-tech/scene/model"
+	"github.com/rhine-tech/scene/model/query"
+)
 
 const Lens scene.CompositionName = "orm"
 
 type ORM interface {
 	OrmName() scene.ImplName
+}
+
+type GenericRepository[Model any] interface {
+	//Create(model ...*Model) error
+	Count(options ...query.Option) (count int64, err error)
+	List(offset, limit int64, options ...query.Option) (model.PaginationResult[Model], error)
+	//Delete(...*Model) (err error)
+	//WithContext(ctx context.Context) RepositoryDriver[Model]
+	//FindOne(options ...query.Option)
 }
