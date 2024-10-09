38b6de6f1fbe90c8217129df810feed237538df6
aynakeya <aynakeya.official@gmail.com>
2024-10-09 05:34:04 -0700

    update must

--- utils/must/must.go
+++ utils/must/must.go
@@ -1,10 +1,10 @@
 package must
 
-func Must[T any](val T, err error) T {
+func Must[T any, E any](val T, err E) T {
 	return val
 }
 
-func Must3[T1 any, T2 any](val1 T1, val2 T2, err error) (T1, T2) {
+func Must3[T1 any, T2 any, E any](val1 T1, val2 T2, err E) (T1, T2) {
 	return val1, val2
 }
 
@@ -20,6 +20,20 @@ func PMust[T any](val T, err error) T {
 func PMust3[T1 any, T2 any](val1 T1, val2 T2, err error) (T1, T2) {
 	if err != nil {
 		panic(err)
+	}
+	return val1, val2
+}
+
+func PMustBool[T any](val T, ok bool) T {
+	if !ok {
+		panic("value not found")
+	}
+	return val
+}
+
+func PMustBool3[T1 any, T2 any](val1 T1, val2 T2, ok bool) (T1, T2) {
+	if !ok {
+		panic("value not found")
 	}
 	return val1, val2
 }
