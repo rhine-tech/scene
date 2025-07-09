5a6024de423db41d3c4442ab4075396d294f12a1
aynakeya <aynakeya.official@gmail.com>
2025-07-09 22:52:15 +0800

    support multi method http route

--- delivery.go
+++ delivery.go
@@ -1,8 +1,47 @@
 package scene
 
+const (
+	HttpMethodGet     uint16 = 0b1
+	HttpMethodHead    uint16 = 0b10
+	HttpMethodPost    uint16 = 0b100
+	HttpMethodPut     uint16 = 0b1000
+	HttpMethodPatch   uint16 = 0b10000 // RFC 5789
+	HttpMethodDelete  uint16 = 0b100000
+	HttpMethodConnect uint16 = 0b1000000
+	HttpMethodOptions uint16 = 0b10000000
+	HttpMethodTrace   uint16 = 0b100000000
+)
+
+// HttpMethod convert http.Method to uint16 version
+func HttpMethod(method string) uint16 {
+	switch method {
+	case "GET":
+		return HttpMethodGet
+	case "HEAD":
+		return HttpMethodHead
+	case "POST":
+		return HttpMethodPost
+	case "PUT":
+		return HttpMethodPut
+	case "PATCH":
+		return HttpMethodPatch
+	case "DELETE":
+		return HttpMethodDelete
+	case "CONNECT":
+		return HttpMethodConnect
+	case "OPTIONS":
+		return HttpMethodOptions
+	case "TRACE":
+		return HttpMethodTrace
+	default:
+		return 0
+	}
+}
+
 type HttpRouteInfo struct {
-	Method string
-	Path   string
+	Method  string // Method specify a single method, might be deprecated in the future
+	Methods uint16 // Methods aim to handle same route with multiple method
+	Path    string
 }
 
 type HttpRoute interface {
--- scenes/gin/router.go
+++ scenes/gin/router.go
@@ -3,7 +3,6 @@ package gin
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/rhine-tech/scene"
-	"net/http"
 )
 
 type Action[T any] interface {
@@ -35,21 +34,33 @@ func (r *AppRouter[T]) Router() gin.IRouter {
 func (r *AppRouter[T]) HandleAction(action Action[T]) {
 	routeInfo := action.GetRoute()
 	chain := MiddlewareChain(action.Middleware()...)
-	switch routeInfo.Method {
-	case http.MethodGet:
-		r.router.GET(routeInfo.Path, chain(Handle(r.app, action))...)
-	case http.MethodPost:
-		r.router.POST(routeInfo.Path, chain(Handle(r.app, action))...)
-	case http.MethodDelete:
-		r.router.DELETE(routeInfo.Path, chain(Handle(r.app, action))...)
-	case http.MethodHead:
-		r.router.HEAD(routeInfo.Path, chain(Handle(r.app, action))...)
-	case http.MethodPut:
-		r.router.PUT(routeInfo.Path, chain(Handle(r.app, action))...)
-	case http.MethodOptions:
-		r.router.OPTIONS(routeInfo.Path, chain(Handle(r.app, action))...)
-	default:
+	methods := routeInfo.Methods | scene.HttpMethod(routeInfo.Method)
+	if methods == 0 {
 		panic("invalid method")
+	}
+	if methods&scene.HttpMethodGet != 0 {
+		r.router.GET(routeInfo.Path, chain(Handle(r.app, action))...)
+	}
+	if methods&scene.HttpMethodHead != 0 {
+		r.router.HEAD(routeInfo.Path, chain(Handle(r.app, action))...)
+	}
+	if methods&scene.HttpMethodPost != 0 {
+		r.router.POST(routeInfo.Path, chain(Handle(r.app, action))...)
+	}
+	if methods&scene.HttpMethodPut != 0 {
+		r.router.PUT(routeInfo.Path, chain(Handle(r.app, action))...)
+	}
+	if methods&scene.HttpMethodPatch != 0 {
+		r.router.PATCH(routeInfo.Path, chain(Handle(r.app, action))...)
+	}
+	if methods&scene.HttpMethodDelete != 0 {
+		r.router.DELETE(routeInfo.Path, chain(Handle(r.app, action))...)
+	}
+	if methods&scene.HttpMethodOptions != 0 {
+		r.router.OPTIONS(routeInfo.Path, chain(Handle(r.app, action))...)
+	}
+	if methods&scene.HttpMethodTrace != 0 {
+		panic("trace method not supported method")
 	}
 }
 
